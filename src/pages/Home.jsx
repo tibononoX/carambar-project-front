18054a751e8799d4e8205bcced1566c5e2c842c4
Thibault C. <thibault-carre@hotmail.fr>
2026-01-26 13:16:55 +0100

    started styling on mobile first landpage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
-import Counter from "@components/Counter";
 import logo from "@assets/Logo.png";
+import carambarjoke from "@assets/carambar-joke.png";
 
 export default function Home() {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Carambar & Co</p>
-    </header>
+    <>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={carambarjoke} className="App-logo" alt="logo" />
+      </header>
+      <div className="display-text">
+        <h2>
+          Les blagues les plus drôles, ici et nulle par ailleurs, offertes par
+          Carambar & Co !
+        </h2>
+      </div>
+    </>
   );
 }
