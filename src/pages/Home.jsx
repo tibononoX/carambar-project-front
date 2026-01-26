ea5db88e6b12029433f79e557e0dc56042c7c8d4
Thibault C. <thibault-carre@hotmail.fr>
2026-01-26 15:27:00 +0100

    added text & fonts & joke paper

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,31 @@
 import logo from "@assets/Logo.png";
 import carambarjoke from "@assets/carambar-joke.png";
+import jokepaper from "@assets/joke0paper.png";
+import button from "@assets/button.png";
 
 export default function Home() {
   return (
     <>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <img src={carambarjoke} className="App-logo" alt="logo" />
+        <img src={carambarjoke} className="App-logo" alt="carambarLogo" />
       </header>
-      <div className="display-text">
-        <h2>
+      <div className="page-content">
+        <h2 className="display-text">
           Les blagues les plus drôles, ici et nulle par ailleurs, offertes par
           Carambar & Co !
         </h2>
+        <div className="joke-paper">
+          <img src={jokepaper} alt="joke" />
+          <p className="joke-text">
+            Quel est le sport le plus fruité ? <br /> <br />
+            Réponse : La boxe, parce que tu te prends des pêches dans la poire
+            et tu tombes dans les pommes.
+          </p>
+        </div>
+        <button className="button">
+          <img src={button} alt="Une autre !" />
+        </button>
       </div>
     </>
   );
