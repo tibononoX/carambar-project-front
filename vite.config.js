68fb352214e85f9997fb387c80590cc510448bdd
Thibault C. <thibault-carre@hotmail.fr>
2026-01-28 18:56:06 +0100

    added deployment configs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/carambar-project-front/",
   plugins: [react()],
   resolve: {
     alias: {
