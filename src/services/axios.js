5c6b40d2fbad57b6301af8db38cf806e11a9c1b2
Thibault C. <thibault-carre@hotmail.fr>
2026-01-28 19:16:08 +0100

    fixes for api

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-
 const apiUrls = import.meta.env.VITE_API_URL?.split(",") || [];
-const baseURL = apiUrls[0] || "http://localhost:8000/api";
+const baseURL = apiUrls[0] || "http://localhost:8000/api/v1";
 
 const instance = axios.create({
   baseURL,
