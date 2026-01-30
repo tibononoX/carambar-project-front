ab800d33f6861d158c71cbd46efa71be754cecbf
Thibault C. <thibault-carre@hotmail.fr>
2026-01-30 14:58:23 +0100

    ajusted some styling for better compatibility on higher resolution monitor, separated footer visuals

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import FooterBg from "@assets/footer.png";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      Test Technique WCS - CDA, Copyright © 2026 Carambar & Co
+      <img className="footer-background" src={FooterBg} alt="Footer Logo" />
+      <p>Test Technique WCS - CDA, Copyright © 2026 Carambar & Co</p>
     </footer>
   );
 };
